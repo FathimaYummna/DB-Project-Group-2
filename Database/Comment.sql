31bbc22b164bba8947f28b37f69d1f97c7c1c4a9
AmnaNisar2003 <amranisar369@gmail.com>
2025-10-12 12:32:08 +0530

    Update Comment.sql

--- Database/Comment.sql
+++ Database/Comment.sql
@@ -1,13 +1,9 @@
 CREATE TABLE Comment (
-    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    Comment_Id CHAR (4) PRIMARY KEY,
     Stu_ID INT NOT NULL,
     Teacher_ID INT NOT NULL,
     comment VARCHAR(400) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (Stu_ID) REFERENCES Student(Stu_ID)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+    FOREIGN KEY (Stu_ID) REFERENCES Student(Stu_ID),
     FOREIGN KEY (Teacher_ID) REFERENCES Teacher(Teacher_ID)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL
 );
